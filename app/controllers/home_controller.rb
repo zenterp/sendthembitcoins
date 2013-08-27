af85e94dcb49e226a1169483685d22b8d8d0749b
Steven Zeiler <zeiler.steven@gmail.com>
2013-08-27 16:42:20 -0700

    [FEATURE] Add table of your gifts over twitter and login.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   layout 'mobile'
   def index
-
+    #if session[:provider] && session[:uid]
+      @gifts = Gift.where(recipient_twitter_username: 'stevenzeiler', network: session[:provider]).reject!{|g| g.funded_at.nil? }
+    #end
+    
+    @gifts = [] if @gifts.nil?
   end
+
 end
