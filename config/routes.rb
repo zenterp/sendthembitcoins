bf2aa4cdd50b5454dc92f157210e1fe907ac5fe6
Steven Zeiler <zeiler.steven@gmail.com>
2013-09-14 19:07:57 -0700

    [FEATURE] Add route to handle coinbase payment callbacks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Sendthembitcoins::Application.routes.draw do
-  root to: 'home#index'
   get 'api', to: 'home#api_docs'
   
   namespace :api do
@@ -10,10 +9,17 @@ Sendthembitcoins::Application.routes.draw do
 
     # user
     get '/user', to: 'user#current'
+
+    # payment callbacks
+    get '/coinbase/payments/success', to: 'gifts#payment_success'
+    get '/coinbase/payments/failure', to: 'gift#payment_failure'
   end
 
   # session management
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'sessions/destroy', to: 'sessions#destroy'
+
+  # client-side javascript app
+  root to: 'home#index'
   get '*path', to: 'home#index'
 end
