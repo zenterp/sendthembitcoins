ce74a98ca445ae7324396b745e727da72ea3bf9b
Steven Zeiler <zeiler.steven@gmail.com>
2013-09-22 02:47:25 -0700

    [FEATURE] Add network scope to Linkedin OAuth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_KEY_SENDTHEMBITCOINS'], ENV['TWITTER_SECRET_SENDTHEMBITCOINS']
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: ''
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
-  provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET']
+  provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET'], scope: 'r_network'
   provider :coinbase, ENV['SENDTHEMBITCOINS_COINBASE_KEY'], ENV['SENDTHEMBITCOINS_COINBASE_SECRET'],
     scope: 'addresses'
 end
