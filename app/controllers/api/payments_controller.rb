3ad1c0bc42b2731d5c0fe0abab59fedd02cb96c7
Steven Zeiler <zeiler.steven@gmail.com>
2013-10-09 04:46:59 -0700

    [FEATURE] Process custom from order params

--- app/controllers/api/payments_controller.rb
+++ app/controllers/api/payments_controller.rb
@@ -2,10 +2,9 @@ class Api::PaymentsController < ApplicationController
   protect_from_forgery :except => [:notification]
   def notification
     if params['order']['status'] == 'completed'
-        custom = JSON.parse(params['order']['custom'])
-        if (gift = Gift.find(gift_id: custom['gift_id'].to_i))
-          gift.update_attributes(funded_at: Time.now)  
-        end
+      custom = JSON.parse(params['order']['custom'])
+      if (gift = Gift.find(gift_id: custom['gift_id'].to_i))
+        gift.update_attributes(funded_at: Time.now)  
       end
     end
     render status: 200
