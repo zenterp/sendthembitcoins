78eccf6bd3a523ec87171f2165d6b3e8e53d53a3
Steven Zeiler <zeiler.steven@gmail.com>
2013-10-09 04:24:52 -0700

    [CHORE] Remove forgery protection for coinbase callback url.

--- app/controllers/api/payments_controller.rb
+++ app/controllers/api/payments_controller.rb
@@ -2,11 +2,12 @@ class Api::PaymentsController < ApplicationController
   protect_from_forgery :except => [:notification]
   def notification
     if params['order']['status'] == 'completed'
-      custom = JSON.parse(params['order']['button']['custom'])
-      if (gift = Gift.find(gift_id: custom['gift_id']))
-        gift.update_attributes(funded_at: Time.now)  
+      if params['order']['button']['custom']
+        custom = JSON.parse(params['order']['button']['custom'])
+        if (gift = Gift.find(gift_id: custom['gift_id'].to_i))
+          gift.update_attributes(funded_at: Time.now)  
+        end
       end
-      
       render status: 200
     end
   end
