bc54a0517d0d296b0a55a5c28aa4ecf74ec63d4f
Steven Zeiler <zeiler.steven@gmail.com>
2013-10-09 04:43:11 -0700

    [FEATURE] Process custom from order params

--- app/controllers/api/payments_controller.rb
+++ app/controllers/api/payments_controller.rb
@@ -1,13 +1,8 @@
 class Api::PaymentsController < ApplicationController
   protect_from_forgery :except => [:notification]
   def notification
-    logger.debug params['order']
     if params['order']['status'] == 'completed'
-      logger.debug params['order']['button']
-      if params['order']['button']['custom']
-        logger.debug params['order']['button']['custom']
-        logger.debug JSON.parse(params['order']['button']['custom'])
-        custom = JSON.parse(params['order']['button']['custom'])
+        custom = JSON.parse(params['order']['custom'])
         if (gift = Gift.find(gift_id: custom['gift_id'].to_i))
           gift.update_attributes(funded_at: Time.now)  
         end
