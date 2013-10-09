3c9a0ea1cc268b61afe847112002040b7e2a3082
Steven Zeiler <zeiler.steven@gmail.com>
2013-10-09 04:32:40 -0700

    [CHORE] Add logging.

--- app/controllers/api/payments_controller.rb
+++ app/controllers/api/payments_controller.rb
@@ -1,9 +1,12 @@
 class Api::PaymentsController < ApplicationController
   protect_from_forgery :except => [:notification]
   def notification
-    binding.pry
+    logger.debug params['order']
     if params['order']['status'] == 'completed'
+      logger.debug params['order']['button']
       if params['order']['button']['custom']
+        logger.debug params['order']['button']['custom']
+        logger.debug JSON.parse(params['order']['button']['custom'])
         custom = JSON.parse(params['order']['button']['custom'])
         if (gift = Gift.find(gift_id: custom['gift_id'].to_i))
           gift.update_attributes(funded_at: Time.now)  
