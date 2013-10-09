27f34f1bb110b931b0569ca2e136ad46d42b3fdf
Steven Zeiler <zeiler.steven@gmail.com>
2013-10-09 03:56:25 -0700

    [FEATURE] Handle coinbase callback post.

--- app/controllers/api/payments_controller.rb
+++ app/controllers/api/payments_controller.rb
@@ -1,7 +1,10 @@
 class Api::PaymentsController < ApplicationController
   def notification
-    render json: {
-      callback: params
-    }
+    if params['order']['status'] == 'completed'
+      invoice_id = params['order']['button']['id']
+      gift = Gift.where(coinbase_invoice_id: invoice_id).first
+      gift.update_attributes(funded_at: Time.now)
+      render status: 200
+    end
   end
 end
