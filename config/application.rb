4f22486e6767ec000e9e9ff385d4683906874440
Steven Zeiler <zeiler.steven@gmail.com>
2013-12-29 15:36:16 -0800

    [CHORE] Only require ssl in production mode

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,6 @@ module Sendthembitcoins
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.force_ssl = true
+    # config.force_ssl = true
   end
 end
