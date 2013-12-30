cf6286a350e5d724633d7fa8f1a7bb99e6efd723
Steven Zeiler <zeiler.steven@gmail.com>
2013-12-29 17:21:44 -0800

    [FEATURE] Make PROVIDERS a constant on SessionAuth class

--- app/models/session_auth.rb
+++ app/models/session_auth.rb
@@ -1,11 +1,19 @@
 class SessionAuth
+  PROVIDERS = [
+    :facebook,
+    :twitter,
+    :github,
+    :linkedin,
+    :coinbase
+  ]
+
   def self.oauth_access_tokens(session)
     auth = self.new(session)
     auth.to_json
   end
 
   def self.clear_access_tokens(session)
-    providers.each do |provider|
+    PROVIDERS.each do |provider|
       session.delete(provider)
     end
   end
@@ -18,13 +26,9 @@ class SessionAuth
     @session = session
   end
 
-  def providers
-    [:twitter, :facebook, :linkedin, :github]
-  end
-
   def to_json
     auth_tokens = {}
-    providers.each do |provider|
+    SessionAuth::PROVIDERS.each do |provider|
       auth = @session[provider]
       if auth.present?
         auth_tokens[provider] = auth
