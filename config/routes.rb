348a099b8fe64af4f848d76838a53880317e2ef1
Steven Zeiler <zeiler.steven@gmail.com>
2013-12-30 20:43:59 -0800

    [BUG] fix config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Sendthembitcoins::Application.routes.draw do
     get '/session/auth', to: 'sessions#index'
     post '/session/clear', to: 'sessions#clear'
 
-    post '/ripple_bridges/:bitcoin_address' to: 'ripple_bridges#create'
+    post '/ripple_bridges/:bitcoin_address', to: 'ripple_bridges#create'
 
     namespace :facebook do
       resources :friends do
