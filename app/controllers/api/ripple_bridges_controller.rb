41052bf16d65d7521939a41c1574b0759828a785
Steven Zeiler <zeiler.steven@gmail.com>
2013-12-30 20:47:21 -0800

    [BUG] Fix typo

--- app/controllers/api/ripple_bridges_controller.rb
+++ app/controllers/api/ripple_bridges_controller.rb
@@ -1,7 +1,7 @@
 class Api::RippleBridgesController < ApplicationController
   # POST /api/ripple_bridges/:bitcoin_address
   def create
-    bridge = RippleBridge.get_bitcoin_bridge(params[:bitcoin_address)
+    bridge = RippleBridge.get_bitcoin_bridge(params[:bitcoin_address])
     render json: bridge.to_json
   end
 end
