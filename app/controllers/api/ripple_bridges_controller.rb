512ddccb3ca3c653061f269a3c41b58e23471511
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-02 21:33:44 -0800

    [FEATURE] Change bridges request from POST to GET

--- app/controllers/api/ripple_bridges_controller.rb
+++ app/controllers/api/ripple_bridges_controller.rb
@@ -1,5 +1,5 @@
 class Api::RippleBridgesController < ApplicationController
-  # POST /api/ripple_bridges/:bitcoin_address
+  # GET /api/ripple_bridges/:bitcoin_address
   def create
     bridge = RippleBridge.get_bitcoin_bridge(params[:bitcoin_address])
     render json: bridge.to_json
