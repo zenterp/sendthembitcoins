932790f2300b652e8db86ab37bb044ad9491a6a7
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-05 15:52:49 -0800

    [FEATURE] Add worker to robustly send payments

---
+++ app/models/ripple/payment_worker.rb
@@ -0,0 +1,16 @@
+module Ripple::PaymentWorker
+  def perform(opts)
+    @invoice  = Invoice.find(opts.delete(:invoice_id))
+    payment = Ripple::Payment.new(opts)
+    ripple_client = Ripple.client({
+      endpoint: ENV['RIPPLED_URL'],
+      client_account: ENV['RIPPLE_ACCOUNT'],
+      client_secret: ENV['RIPPLE_SECRET']
+    })
+
+    transaction_hash = payment.submit_with(ripple_client)
+    @invoice.ripple_tx_hash = transaction_hash
+    @invoice.ripple_tx_status = 'success'
+    @invoice.save
+  end
+end
