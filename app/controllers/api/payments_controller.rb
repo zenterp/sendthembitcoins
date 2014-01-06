083367e4fe03c3e93820144b683907c4d549965e
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-05 16:47:13 -0800

    [FEATURE] Configure coinbase payment callback handler to process ripple bridge payments

--- app/controllers/api/payments_controller.rb
+++ app/controllers/api/payments_controller.rb
@@ -1,12 +1,29 @@
 class Api::PaymentsController < ApplicationController
   protect_from_forgery :except => [:notification]
+  before_filter :verify_completed
   def notification
-    if params['order']['status'] == 'completed'
-      custom = JSON.parse(params['order']['custom'])
-      if (gift = Gift.find(custom['gift_id'].to_i))
-        gift.update_attributes(funded_at: Time.now)  
-      end
+    custom = JSON.parse(params['order']['custom'])
+    if custom['invoice_id']
+      handle_ripple_payment(custom)
+    elsif (gift = Gift.find(custom['gift_id'].to_i))
+      gift.update_attributes(funded_at: Time.now)  
     end
     render status: 200
+  end
+
+  def verify_completed
+    params['order']['status'] == 'completed'
+  end
+
+  def handle_ripple_payment
+    invoice = Invoice.find(invoice_id)
+    if invoice.secret == custom['secret']
+      Rescue.enqueue(Ripple::PaymentWorker, {
+        destination: invoice.bitcoin_address,
+        currency: 'BTC',
+        amount: invoice.amount
+      })  
+    end
+
   end
 end
