aa3457b96feb71404b447b6fbd89c0c31a000a93
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-05 17:03:02 -0800

    [BUG] fix validation call in ripple bridge invoice

--- app/models/ripple_bridge_invoice.rb
+++ app/models/ripple_bridge_invoice.rb
@@ -1,7 +1,7 @@
 class RippleBridgeInvoice < ActiveRecord::Base
   attr_accessible :amount, :ripple_address, :coinbase_invoice_id, :currency, :funded, :ripple_tx_hash, :ripple_tx_status, :secret
 
-  validates_presences_of :amount, :ripple_address, :currency, :secret, :coinbase_invoice_id
+  validates_presence_of :amount, :ripple_address, :currency, :secret, :coinbase_invoice_id
 
   before_create :set_random_secret
 
