a3272115052871fc547c2bd7599e2ef017176437
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-07 22:02:34 -0800

    [FEATURE] Subclass BridgebaseController

--- app/controllers/bridgebase/onboard_controller.rb
+++ app/controllers/bridgebase/onboard_controller.rb
@@ -1,8 +1,6 @@
-class Bridgebase::OnboardController < ApplicationController 
-  layout 'bridgebase'
+class Bridgebase::OnboardController < BridgebaseController 
+  skip_before_filter :verify_coinbase_session
     
-
   def home
-        
   end
 end
