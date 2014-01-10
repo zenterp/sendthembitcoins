90343ffb08ad0f26c477bfe98ad015f76f67d99b
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-09 19:40:26 -0800

    [FEATURE] Add bridgebase gateway account balances.

--- app/controllers/bridgebase/accounts_controller.rb
+++ app/controllers/bridgebase/accounts_controller.rb
@@ -1,10 +1,27 @@
 class Bridgebase::AccountsController < BridgebaseController
+  before_filter :ensure_gateway_account, only: :show
+
   def show
-  end 
+    @balances = client.get_gateway_balances(@user_id)
+  end
 
   def logout
     reset_session
     session[:coinbase] = nil
     redirect_to '/bridgebase'
+  end
+
+  def ensure_gateway_account
+    @user_id = session[:coinbase][:uid]
+    @gateway_account = client.get_gateway_account(@user_id)
+    if !@gateway_account
+      @gateway_account = client.create_gateway_account(@user_id)
+    end
+  end
+
+private
+
+  def gateway_client
+    @api ||= ripple_gateway_client
   end
 end
