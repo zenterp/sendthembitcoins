a371ae8f03a71ad7c4fcc9a726faf0d63615b83a
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-09 19:01:39 -0800

    [FEATURE] Add methods to create user, gateway account.

--- lib/ripple/gateway/client.rb
+++ lib/ripple/gateway/client.rb
@@ -7,9 +7,20 @@ module Ripple
       def initialize(opts)
         @api = opts[:gateway_api_url] || 'localhost:4000'
       end
+
+      def create_user(name, password)
+        self.class.post("#{@api}/v1/gateway/users", { body: {
+          name: name, password: password
+        }}).parsed_response
+      end
     
       def get_gateway_account(user_id)
         self.class.get("#{@api}/v1/gateway/users/#{user_id}/gateway_account").
+          parsed_response
+      end
+    
+      def create_gateway_account(user_id)
+        self.class.post("#{@api}/v1/gateway/users/#{user_id}/gateway_accounts").
           parsed_response
       end
     end 
