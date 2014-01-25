7dcaa09e2fbd989935b352266fbe741d11212c3c
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-24 21:39:25 -0800

    [FEATURE] Add validations for required fields of escrow table.

--- app/models/escrow.rb
+++ app/models/escrow.rb
@@ -1,3 +1,4 @@
 class Escrow < ActiveRecord::Base
   attr_accessible :accepted_at, :amount, :auth_provider, :auth_uid, :currency, :funded_at
+  validates_presence_of :currency, :amount, :auth_uid, :auth_provider
 end
