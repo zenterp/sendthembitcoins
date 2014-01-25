42dc85c445769ecfcfb7fe1ebaf9fbd729173627
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-24 23:14:44 -0800

    [BUG] Use proper strong_parameters require syntax

--- app/controllers/api/escrows_controller.rb
+++ app/controllers/api/escrows_controller.rb
@@ -6,20 +6,20 @@ class Api::EscrowsController < ApplicationController
 
   def index
     @escrows = Escrow.where(
-      auth_provider: params.require[:auth_provider], 
-      auth_uid: params.require[:auth_uid]
+      auth_provider: params.require(:auth_provider), 
+      auth_uid: params.require(:auth_uid)
     )
     render json: @escrows
   end
 
   def create
     @escrow = Escrow.create(
-      auth_provider: params.require[:auth_provider],
-      auth_uid: params.require[:auth_uid],
-      amount: params.require[:amount],
-      currency: params.require[:currency],
-      invoice_id: params[:invoice_id],
-      invoice_provider: params[:invoice_provider]
+      auth_provider: params.require(:auth_provider),
+      auth_uid: params.require(:auth_uid),
+      amount: params.require(:amount),
+      currency: params.require(:currency),
+      invoice_id: params(:invoice_id),
+      invoice_provider: params(:invoice_provider)
     )
     render json: @escrow
   end
