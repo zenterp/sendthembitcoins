7483cf566bbf701d9b1e969868ac236ce55e1235
Steven Zeiler <zeiler.steven@gmail.com>
2014-01-27 01:34:12 -0800

    [FEATURE] Add accept controller action for escrows, methods for each auth provider.

--- app/controllers/api/escrows_controller.rb
+++ app/controllers/api/escrows_controller.rb
@@ -12,6 +12,23 @@ class Api::EscrowsController < ApplicationController
     render json: @escrows
   end
 
+  def accept
+    access_token = params.require(:access_token)
+    bitcoin_address = params.require(:bitcoin_address)
+    @escrow = Escrow.find(params[:id])
+
+    case @escrow.auth_provider
+    when 'twitter' 
+      accept_twitter
+    when 'linkedin'
+      accept_linkedin
+    when 'facebook'
+      accept_facebook
+    when 'github'
+      accept_github
+    end
+  end
+
   def create
     @escrow = Escrow.create(
       auth_provider: params.require(:auth_provider),
@@ -20,5 +37,32 @@ class Api::EscrowsController < ApplicationController
       currency: params.require(:currency)
     )
     render json: { invoice_url: "https://coinbase.com/checkouts/#{@escrow.invoice_id}" }
+  end
+
+private
+
+  def accept_twitter
+    access_token_secret = params.require(:access_token_secret)
+    verifier = OauthVerifier::Twitter.new(
+      consumer_key: ENV['TWITTER_KEY_SENDTHEMBITCOINS'],
+      consumer_secret: ENV['TWITTER_SECRET_SENDTHEMBITCOINS']
+    )
+    if verifier.validate(@escrow.auth_uid, access_token, access_token_secret)
+      @escrow.accept
+    end
+    render json: @escrow
+  end
+  
+  def accept_github
+    render json: @escrow
+  end
+  
+  def accept_facebook
+    render json: @escrow
+  end
+  
+  def accept_linkedin
+    access_token_secret = params.require(:access_token_secret)
+    render json: @escrow
   end
 end
